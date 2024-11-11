ff4b43164f35465614a1d4be5f90a67869fab3b6
invisible-gaopg <187073425+invisible-gaopg@users.noreply.github.com>
2024-11-11 23:41:59 +0800

    Update

---
+++ update.sh
@@ -0,0 +1,9 @@
+git config --global user.email "187073425+invisible-gaopg@users.noreply.github.com"
+git config --global user.name "invisible-gaopg"
+gh auth switch -u invisible-gaopg
+git add .
+git commit -m "Update"
+git push
+git config --global user.email "81460770+Davidasx@users.noreply.github.com"
+git config --global user.name "Davidasx"
+gh auth switch -u Davidasx
