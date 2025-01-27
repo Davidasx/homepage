64c96b9c92a845a7799a1af0308a1e04e3b4b8e5
Davidasx <81460770+Davidasx@users.noreply.github.com>
2025-01-27 15:06:33 +0800

    Blog Update

--- update.sh
+++ update.sh
@@ -1,9 +1,3 @@
-git config --global user.email "190557641+DavidBloggerTest@users.noreply.github.com"
-git config --global user.name "DavidBloggerTest"
-gh auth switch -u DavidBloggerTest
 git add .
 git commit -m "Blog Update"
 git push
-git config --global user.email "81460770+Davidasx@users.noreply.github.com"
-git config --global user.name "Davidasx"
-gh auth switch -u Davidasx
